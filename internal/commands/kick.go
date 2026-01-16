6ee08b2d26b263b8d5922875b66344e996120840
Mozoa <dylan@mozoa.net>
2026-01-16 07:02:29 +0100

    restructure kick command to seperate embed sending and kick logic

--- internal/commands/kick.go
+++ internal/commands/kick.go
@@ -10,30 +10,36 @@ import (
 )
 
 func handleKick(event *events.ApplicationCommandInteractionCreate) {
-	data := event.SlashCommandInteractionData()
-	user := data.Member("member")
-	reason, reasonSet := data.OptString("reason")
-	var embed *discord.EmbedBuilder
-	if !reasonSet {
-		reason = "n/a"
-	}
-	if kickErr := event.Client().Rest().RemoveMember(*event.GuildID(), user.User.ID); kickErr != nil {
-		embed = discord.NewEmbedBuilder().
-			SetTitle("Error kicking member").
-			SetDescriptionf("Couldn't kick %s, check if the user is still in the server, if %s has enough permissions to kick them and if its role is above the targeted user", user.EffectiveName(), os.Getenv("BOT_NAME")).
-			SetColor(common.ColourError)
-	} else {
-		embed = discord.NewEmbedBuilder().
-			SetTitle("Member kicked").
-			AddField("Name:", user.EffectiveName(), false).
-			AddField("Reason", reason, false).
-			AddField("Kicked by", event.User().Username, false).
-			SetColor(common.ColourSuccess)
-	}
+
+	embed := tryKickOrFail(event)
+
 	if err := event.CreateMessage(
 		discord.NewMessageCreateBuilder().AddEmbeds(embed.Build()).Build(),
 	); err != nil {
 		slog.Error("Error responding to interaction", slog.Any("err", err))
 	}
 
+}
+
+func tryKickOrFail(event *events.ApplicationCommandInteractionCreate) *discord.EmbedBuilder {
+	data := event.SlashCommandInteractionData()
+	user := data.Member("member")
+	reason, reasonSet := data.OptString("reason")
+	if !reasonSet {
+		reason = "n/a"
+	}
+
+	if kickErr := event.Client().Rest().RemoveMember(*event.GuildID(), user.User.ID); kickErr != nil {
+		return discord.NewEmbedBuilder().
+			SetTitle("Error kicking member").
+			SetDescriptionf("Couldn't kick %s, check if the user is still in the server, if %s has enough permissions to kick them and if its role is above the targeted user", user.EffectiveName(), os.Getenv("BOT_NAME")).
+			SetColor(common.ColourError)
+	}
+
+	return discord.NewEmbedBuilder().
+		SetTitle("Member kicked").
+		AddField("Name:", user.EffectiveName(), false).
+		AddField("Reason", reason, false).
+		AddField("Kicked by", event.User().Username, false).
+		SetColor(common.ColourSuccess)
 }
